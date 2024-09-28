40324ee3807d03f279a8ff0b10fb78ad60c534a4
Meerim Maratova <maratovameerim@gmail.com>
2024-09-28 10:13:46 +0600

    changed buttons' style

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,11 +3,15 @@ import {IngredientButtonType} from "./types.ts";
 
 const IngredientButton = ({ingredient, addIngredient, deleteIngredient} : IngredientButtonType) => {
     return (
-                <div key={ingredient.name} className="buttonDiv">
-                    <img className="buttonImg" src={ingredient.image} alt="ingredients"/>
-                    <button type="button" onClick={() => addIngredient(ingredient.name)}>{ingredient.name}</button>
-                    <button className="deleteBtn" type="button" onClick={() => deleteIngredient(ingredient.name)}>❌</button>
-                </div>
+        <div key={ingredient.name} className="buttonDiv">
+            <button className="addBtn" type="button" onClick={() => addIngredient(ingredient.name)}>
+                <img className="buttonImg" src={ingredient.image} alt="ingredients"/>
+                {ingredient.name}
+            </button>
+            <button className="deleteBtn" type="button"
+                    onClick={() => deleteIngredient(ingredient.name)}>❌
+            </button>
+        </div>
     )
 };
 
